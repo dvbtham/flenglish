4f7fb8313f7bbe45b614d28393df4403c10fcb29
Tham <thamdavies@gmail.com>
2019-01-19 19:04:19 +0700

    Search movies for client side.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,14 @@
 class MoviesController < ApplicationController
   def show; end
 
-  def search; end
+  def search
+    @search = Search.new
+    search_params = params.slice :term, :level, :genre, :category, :column_sort
+    @search.movies = Movie.filter(search_params).paginate(page: params[:page],
+      per_page: Settings.home.movies)
+    @search.genres = Genre.key_value_pairs
+    @search.levels = Level.key_value_pairs
+    @search.categories = Category.key_value_pairs
+    @search.sort_by = Movie.filterable_columns
+  end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,6 @@
 class Genre < ApplicationRecord
   has_many :movies
   has_and_belongs_to_many :movies
+
+  scope :key_value_pairs, ->{pluck :name, :id}
 end
