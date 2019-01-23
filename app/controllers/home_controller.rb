f126fef2ae3e1c873f719a5ac5ca55bdca7cac59
Tham <thamdavies@gmail.com>
2019-01-23 09:21:15 +0700

    fix movie items layouts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @home = Home.new
-    @home.feature_movies = Movie.features.newest.paginate(page: params[:page],
+    @feature_movies = Movie.features.newest.paginate(page: params[:page],
       per_page: Settings.home.movies)
-    @home.levels = Level.includes :movies
+    @levels = Level.includes :movies
     respond_to do |format|
       format.html
-      format.json{render json: @home.feature_movies}
+      format.json{render json: @feature_movies}
       format.js
     end
   end
